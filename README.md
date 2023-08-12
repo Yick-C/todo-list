585b7c570578c2e2e6f7742f867ef751999fad3e
Christina <yickind@hotmail.co.uk>
2023-08-12 20:26:31 +0100

    Added database and updated CRUD in pages

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# todo-list
+# ToDo List
+- The application's web server is built using Node.js and Express.js which handles routing and middleware.
+- EJS is used as the templating engine to generate dynamic HTML based on the application's state
--- index.js
+++ index.js
@@ -1,11 +1,53 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import _ from 'lodash';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static("public"));
+
+// Setup items database and record schema
+mongoose.connect(process.env.MONGODB_URL)
+.then(function(){
+    console.log("Connected successfully to the database!");
+})
+.catch(function(){
+    console.log("Database connection failed");
+});
+
+// Create schema, model and default items
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Press the + button to add a new item"
+});
+
+const item3 = new Item({
+    name: "<-- Press this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Create list schema and model
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", (req, res) => {
     const d = new Date();
@@ -13,23 +55,95 @@ app.get("/", (req, res) => {
     let month = months[d.getMonth()];
     let date = d.getDate();
     let todaysDate = `${day}, ${month} ${date}`;
-    res.render("index.ejs", {nowDate: todaysDate, allNotes: notes});
-})
 
-app.get("/work", (req, res) => {
-    res.render("work.ejs", {allWorkNotes: workNotes});
+    Item.find({}).then(function(allItems) {
+        if(allItems.length == 0){
+            Item.insertMany(defaultItems)
+            .then(function() {
+                console.log("Default items inserted successfully!");
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+            res.redirect("/");
+        }
+        else {
+            res.render("index", {listTitle: todaysDate, allNotes: allItems});
+        }
+    })
 })
 
 app.post("/", (req, res) => {
-    var note = req.body.newNote;
-    notes.push(note);
-    res.redirect(("/"));
+    const note = req.body.newNote;
+    const listName = req.body.list;
+
+    console.log(note);
+    console.log(listName);
+    const newItem = new Item({
+        name: note
+    });
+
+    if(listName === 'Today') {
+        newItem.save();
+        res.redirect("/");
+    }
+    else {
+        List.findOne({name: listName})
+        .then(function(foundList) {
+            foundList.items.push(newItem);
+            foundList.save();
+            res.redirect("/" + listName);
+        })
+    }
+    
+
 })
 
-app.post("/work", (req, res) => {
-    var workNote = req.body.newWorkNote;
-    workNotes.push(workNote);
-    res.redirect(("/work"));
+app.get("/:customListName", (req, res) => {
+    const customURL = _.capitalize(req.params.customListName);
+
+    List.findOne({name: customURL})
+    .then (function(foundList) {
+        if(!foundList) {
+            console.log("New custom list created");
+            const list = new List({
+                name: customURL,
+                items: defaultItems
+            });
+            list.save();
+            res.redirect("/" + customURL);
+        }
+        else {
+            res.render("list", {listTitle: foundList.name, allNotes: foundList.items} )
+        }
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+})
+
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    const listName = req.body.list;
+
+    if(listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId)
+        .then(function() {
+            res.redirect("/");
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+    }
+    else {
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}})
+        .then(function(foundList) {
+            res.redirect("/" + listName);
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+    }
 })
 
 app.listen(port, () => {
@@ -38,5 +152,3 @@ app.listen(port, () => {
 
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var notes = [];
-var workNotes = [];
